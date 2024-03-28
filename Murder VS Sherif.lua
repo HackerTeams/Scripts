8f3a69b0a24fb1ae4d8652464813addda4076f1f
Secrets <162525797+HackerTeams@users.noreply.github.com>
2024-03-28 16:10:06 +0700

    Murder VS Sherif.lua

--- Murder VS Sherif.lua
+++ Murder VS Sherif.lua
@@ -16,7 +16,7 @@ wait(1999 -(1805 + 193) )
 v0:Notify("Loaded")
 v0:Notify("Welcome to Hacker Hub | Welcome : "   .. game.Players.LocalPlayer.DisplayName )
 
-local v44 = v0:CreateWindow({Title="Hacker Hub | Murder VS Sherif Duels | Welcome : "   .. game.Players.LocalPlayer.DisplayName ,Center=true,AutoShow=true,TabPadding=5,MenuFadeTime=0.2 + 0 })
+local v44 = v0:CreateWindow({Title="Hacker Hub | Murder VS Sherif Duels | Welcome : "   .. game.Players.LocalPlayer.DisplayName, Center=true,AutoShow=true,TabPadding=5,MenuFadeTime=0.2 + 0 })
 local v45={Main=v44:AddTab("Main")}
 
 local v46 = v45.Main:AddLeftGroupbox("[>LocalPlayer<]")
@@ -46,15 +46,6 @@ local function unleashYourPower()
     if humanoid then
         humanoid.WalkSpeed = 100
     end
-end
-
-speedButton.MouseButton1Click:Connect(unleashYourPower)
-end})
-
-v46:AddButton({Text="Reset Speed",Func=function()
-    frame.Active = false
-    return
-    humanoid.WalkSpeed = 16
 end
 
 speedButton.MouseButton1Click:Connect(unleashYourPower)
@@ -93,4 +84,54 @@ local v54=v46:AddButton({Text="Hacker Admin",Func=function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Hacker3331/HackerHub-Games/main/HackerAdminGames.lua"))()
 end})
 
-local v45={["UI Settings"]=v44:AddTab("UI Settings")};local v55=tick();local v56=0 -0 ;local v57=1958 -(41 + 1857) ;local v58=game:GetService("RunService").RenderStepped:Connect(function()local v223=1893 -(1222 + 671) ;while true do if (v223==(0 -0)) then v56+=(1 -0) if ((tick() -v55)>=(1183 -(229 + 953))) then local v504=1774 -(1111 + 663) ;while true do if (v504==(1580 -(874 + 705))) then v56=0 + 0 ;break;end if (v504==(0 + 0)) then v57=v56;v55=tick();v504=1 -0 ;end end end v223=1 + 0 ;end if (v223==(680 -(642 + 37))) then v0:SetWatermark(("Hacker Hub | Murder VS Sherif Duels | %s fps | %s ms"):format(math.floor(v57),math.floor(game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue())));break;end end end);v0:OnUnload(function()v58:Disconnect();print("Unloaded!");v0.Unloaded=true;end);local v59=v45["UI Settings"]:AddLeftGroupbox("Menu");v59:AddButton("Unload",function()v0:Unload();end);v59:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind",{Default="End",NoUI=true,Text="Menu keybind"});v0.KeybindFrame.Visible=false;_G.ShowKb=false;v59:AddToggle("MyToggle",{Text="Show Keybind",Default=false,Tooltip="If you enable Show Keybind it then if u disable it.",Callback=function(v225)_G.ShowKb=v225;if (_G.ShowKb==false) then v0.KeybindFrame.Visible=false;elseif (_G.ShowKb==true) then v0.KeybindFrame.Visible=true;end end});v0.ToggleKeybind=Options.MenuKeybind;v1:SetLibrary(v0);v2:SetLibrary(v0);v2:IgnoreThemeSettings();v2:SetIgnoreIndexes({"MenuKeybind"});v1:SetFolder("MyScriptHub");v2:SetFolder("MyScriptHub/specific-game");v2:BuildConfigSection(v45["UI Settings"]);v1:ApplyToTab(v45["UI Settings"]);v2:LoadAutoloadConfig();v0:Notify("[Notifier] Press End To Close And Open");
+local v55 = tick()
+local v56 = 0
+local v57 = 1958 -(41 + 1857)
+local v58 = game:GetService("RunService").RenderStepped:Connect(function()
+    local v223=1893 -(1222 + 671)
+    while true do if (v223 == (0 -0)) then
+        v56+=(1 -0) if ((tick() -v55)>=(1183 -(229 + 953))) then
+            local v504 = 1774 -(1111 + 663)
+            while true do if (v504==(1580 -(874 + 705))) then
+                v56=0 + 0 
+                break
+            end
+        end
+    end
+end
+if (v504==(0 + 0)) then 
+    v57 = v56
+    v55 = tick()
+    v504 = 1 - 0
+end
+
+v223=1 + 0 
+if (v223==(680 -(642 + 37))) then
+    v0:SetWatermark(("Hacker Hub | Murder VS Sherif Duels | %s fps | %s ms"):format(math.floor(v57),math.floor(game:GetService("Stats").Network.ServerStatsItem["Data Ping"]:GetValue())))
+    break
+        end
+    end
+end)
+
+v59:AddLabel("Menu bind"):AddKeyPicker("MenuKeybind",{Default="End",NoUI=true,Text="Menu keybind"})
+v0.KeybindFrame.Visible = false
+_G.ShowKb = false
+
+v59:AddToggle("MyToggle",{Text="Show Keybind",Default=false,Tooltip="If you enable Show Keybind it then if u disable it.",Callback=function(v225)_G.ShowKb=v225
+    if (_G.ShowKb == false) then
+        v0.KeybindFrame.Visible = false
+    elseif (_G.ShowKb == true) then
+        v0.KeybindFrame.Visible = true
+    end
+end})
+
+v0.ToggleKeybind = Options.MenuKeybind
+v1:SetLibrary(v0)
+v2:SetLibrary(v0)
+v2:IgnoreThemeSettings()
+v2:SetIgnoreIndexes({"MenuKeybind"})
+v1:SetFolder("MyScriptHub")
+v2:SetFolder("MyScriptHub/specific-game")
+v2:BuildConfigSection(v45["UI Settings"])
+v2:LoadAutoloadConfig()
+v0:Notify("[Notifier] Press End To Close And Open");
